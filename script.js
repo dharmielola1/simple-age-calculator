321c19ed5b165d28952a451b8941d10a4604d337
unknown <dharmielola@gmail.com>
2023-08-05 08:35:36 +0100

    add project files and setup

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-const prompt = require ("prompt-sync")();
 
-let a = parseInt (prompt("Insert first number"));
-leb b = parseInt (prompt("Insert second number"));
 
-function addition() {
-    return  a + b;
-}
-   
-
-console.log(a);
