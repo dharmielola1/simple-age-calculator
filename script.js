2e3beb756d4df0996ce4f0bfef16001faf5d2326
unknown <dharmielola@gmail.com>
2023-08-05 08:55:41 +0100

    create pseudo code for the calculator

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-
+//create variables to accept first and second user imput 
+//create an addition function 
+//addition function will return the addition for imput a and b
+//invoke addition function to run the calculator
 
